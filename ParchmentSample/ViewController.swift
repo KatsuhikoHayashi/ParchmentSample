9ba70b86f262c580f930ed2df2aa0b1a1e4c23b1
Katsuhiko Hayashi <katsuhiko.hayashi.847@gmail.com>
2018-09-25 20:31:01 +0900

    Initial Commit

---
+++ ParchmentSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParchmentSample
+//
+//  Created by 林克彦 on 2018/09/25.
+//  Copyright © 2018年 Katsuhiko Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
