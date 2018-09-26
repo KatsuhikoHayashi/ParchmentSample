75bb40df7c4a426bd9c721f9c3c632eb9636ffe5
Katsuhiko Hayashi <katsuhiko.hayashi.847@gmail.com>
2018-09-26 10:17:52 +0900

    ファイル足りてなかった

---
+++ ParchmentSample/ViewController2.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController2.swift
+//  ParchmentSample
+//
+//  Created by 林克彦 on 2018/09/25.
+//  Copyright © 2018年 Katsuhiko Hayashi. All rights reserved.
+//
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "コードで書いてる"
+    }
+   
+}
